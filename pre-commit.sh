8ab7f3f5812c99adae06c56f5c782af3750439d9
samuel-jimenez <53528911+samuel-jimenez@users.noreply.github.com>
2026-01-12 18:00:38 -0600

    ci: edit precommit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,26 +1,34 @@
 #!/bin/sh
 # pre-commit.sh
 
-#stash changes
 
+#stash changes
 STASH_NAME="pre-commit-$(date +%s)"
 git stash save --quiet --keep-index --include-untracked $STASH_NAME
 
-
-
 # format
-cargo +nightly fmt -- --config imports_granularity="Crate",group_imports="StdExternalCrate"
-#re add
-git add .
-
-# Test prospective commit
-./run_tests.sh
+cargo +nightly fmt --check --quiet -- --config imports_granularity="Crate",group_imports="StdExternalCrate"
 RESULT=$?
+cargo +nightly fmt -- --config imports_granularity="Crate",group_imports="StdExternalCrate"
 
 STASHES=$(git stash list)
+if [[ $RESULT -ne 0 ]]; then
+  if [[ $STASHES == *"$STASH_NAME"* ]]; then
+    git stash pop --quiet
+  fi
+  echo "Files formatted. Re-add and try again."
+  exit 1
+fi
+
+
+
+# Test prospective commit
+# ./run_tests.sh
+# RESULT=$?
+
 if [[ $STASHES == *"$STASH_NAME"* ]]; then
   git stash pop --quiet
 fi
 
-[ $RESULT -ne 0 ] && exit 1
+# [ $RESULT -ne 0 ] && exit 1
 exit 0
